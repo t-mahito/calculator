981c4fe47c6f45c24d122eb46685d635e2567718
t-mahito <qqna42p9@gmail.com>
2022-09-21 00:27:26 +0900

    route 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 root to: "calc#index"
 
-resources :calc ,only:[:index,:new,:create]
+resources :calc ,only:[:index,:new,:create] do
+  collection do
+    get "main"
+  end
+ end
 end
